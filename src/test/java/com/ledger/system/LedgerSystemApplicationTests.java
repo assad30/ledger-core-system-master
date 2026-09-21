6f5120cd7726349fb0652a79ee115760b88101c2
Assadullah Buriro <assadullah.buriro@easypaisa.com.pk>
2026-09-21 16:20:39 +0500

    Added Logger to Test Jenkins Logs

--- src/test/java/com/ledger/system/LedgerSystemApplicationTests.java
+++ src/test/java/com/ledger/system/LedgerSystemApplicationTests.java
@@ -2,13 +2,18 @@ package com.ledger.system;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class LedgerSystemApplicationTests {
 
+	public static Logger logger = LoggerFactory.getLogger(LedgerSystemApplicationTests.class);
+
 	@Test
 	public void contextLoads() {
+		logger.info("Test case execution started with Assad..");
 		Assertions.assertTrue(true);
 	}
 
